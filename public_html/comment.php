26c6ffc23c497c5d2eb013ce6757259b230d11f2
dhaun <dhaun@localhost>
2004-05-31 12:49:51 +0000

    Fix for the anonymous comment spamming even if comments for anonymous users were switched off. Also added an additional check for the comment speed limit.

--- public_html/comment.php
+++ public_html/comment.php
@@ -32,7 +32,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 //
-// $Id: comment.php,v 1.44.2.4 2004/01/23 16:55:24 dhaun Exp $
+// $Id: comment.php,v 1.44.2.5 2004/05/31 12:49:51 dhaun Exp $
 
 /**
 * This file is responsible for letting user enter a comment and saving the
@@ -229,10 +229,29 @@ function savecomment($uid,$title,$comment,$sid,$pid,$type,$postmode)
 
     $retval = '';
 
-    if (($uid < 1) || (($uid != $_USER['uid']) && !empty ($_USER['username'])) ||
-            (empty ($_USER['username']) && (($_CONF['loginrequired'] == 1) ||
-            ($_CONF['commentsloginrequired'] == 1)))) {
+    // ignore $uid as it may be manipulated anyway
+    if (empty ($_USER['uid'])) {
+        $uid = 1;
+    } else {
+        $uid = $_USER['uid'];
+    }
+
+    if (empty ($sid) || empty ($title) || empty ($comment) || empty ($type) ||
+            (($uid == 1) && (($_CONF['loginrequired'] == 1) ||
+                ($_CONF['commentsloginrequired'] == 1)))) {
         $retval .= COM_refresh ($_CONF['site_url'] . '/index.php');
+        return $retval;
+    }
+
+    // Check for people breaking the speed limit
+    COM_clearSpeedlimit ($_CONF['commentspeedlimit'], 'comment');
+    $last = COM_checkSpeedlimit ('comment');
+    if ($last > 0) {
+        $retval .= COM_startBlock ($LANG12[26], '', COM_getBlockTemplate ('_msg_block', 'header'))
+                . $LANG03[7]
+                . $last
+                . $LANG03[8]
+                . COM_endBlock (COM_getBlockTemplate ('_msg_block', 'footer'));
         return $retval;
     }
 

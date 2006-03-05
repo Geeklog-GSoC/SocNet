69244e4b2e65e08c05e981609d11c28a84508691
dhaun <dhaun@localhost>
2006-03-05 09:07:59 +0000

    Ported security fix over from the trunk

--- config.php
+++ config.php
@@ -33,7 +33,7 @@
 // | on configuration.                                                         |
 // +---------------------------------------------------------------------------+
 //
-// $Id: config.php,v 1.208 2006/02/19 10:52:26 dhaun Exp $
+// $Id: config.php,v 1.208.2.1 2006/03/05 09:07:59 dhaun Exp $
 
 // When setting up Geeklog for the first time, you need to make sure the
 // settings in the following 3 sections are correct:
@@ -808,7 +808,7 @@ if (!defined ('LB')) {
     define('LB',"\n");
 }
 if (!defined ('VERSION')) {
-    define('VERSION', '1.4.0sr1');
+    define('VERSION', '1.4.0sr2');
 }
 
 $_STATES = array(

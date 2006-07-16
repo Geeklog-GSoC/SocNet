353d0af2226b0b0c577d416612036beaa387bbaa
dhaun <dhaun@localhost>
2006-07-16 07:57:38 +0000

    Updated version number and changelog

--- config.php
+++ config.php
@@ -33,7 +33,7 @@
 // | on configuration.                                                         |
 // +---------------------------------------------------------------------------+
 //
-// $Id: config.php,v 1.208.2.4 2006/06/30 14:54:54 dhaun Exp $
+// $Id: config.php,v 1.208.2.5 2006/07/16 07:57:38 dhaun Exp $
 
 // When setting up Geeklog for the first time, you need to make sure the
 // settings in the following 3 sections are correct:
@@ -815,7 +815,7 @@ if (!defined ('LB')) {
     define('LB',"\n");
 }
 if (!defined ('VERSION')) {
-    define('VERSION', '1.4.0sr4');
+    define('VERSION', '1.4.0sr5');
 }
 
 $_STATES = array(

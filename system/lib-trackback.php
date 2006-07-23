d5ed58214b0c45174b4bb9ac2853e809e80c3082
dhaun <dhaun@localhost>
2006-07-23 07:35:33 +0000

    Wrong variable name

--- system/lib-trackback.php
+++ system/lib-trackback.php
@@ -29,7 +29,7 @@
 // |                                                                           |
 // +---------------------------------------------------------------------------+
 // 
-// $Id: lib-trackback.php,v 1.23.2.2 2006/06/30 12:20:48 dhaun Exp $
+// $Id: lib-trackback.php,v 1.23.2.3 2006/07/23 07:35:33 dhaun Exp $
 
 if (strpos ($_SERVER['PHP_SELF'], 'lib-trackback.php') !== false) {
     die ('This file can not be used on its own!');
@@ -461,7 +461,7 @@ function TRB_linksToUs ($sid, $type, $urlToGet)
     $req = new HTTP_Request ($urlToGet);
     $req->addHeader ('User-Agent', 'GeekLog/' . VERSION);
     if (PEAR::isError ($req->sendRequest ())) {
-        COM_errorLog ("Trackback verification: " . $response->getMessage()
+        COM_errorLog ("Trackback verification: " . $req->getMessage()
                       . " when requesting $urlToGet");
     } else {
         if ($req->getResponseCode () == 200) {

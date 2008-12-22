8d76fab912b80b56985411649f129de1d3ebd615
Dirk Haun <dirk@haun-online.de>
2008-12-22 12:00:58 +0100

    Fixed STORY_getItemInfo - need to check the draft flag and for a publish date in the future

--- system/lib-story.php
+++ system/lib-story.php
@@ -727,6 +727,7 @@ function STORY_getItemInfo ($sid, $what)
     if (count ($fields) > 0) {
         $result = DB_query ("SELECT " . implode (',', $fields)
                     . " FROM {$_TABLES['stories']} WHERE sid = '$sid'"
+                    . ' AND (draft_flag = 0) AND (date <= NOW())';
                     . COM_getPermSql ('AND') . COM_getTopicSql ('AND'));
         $A = DB_fetchArray ($result);
     } else {

e4a3e7d6ca8287c480c7b5fd935c43d6048ba994
stan <stan@spalatnik.com>
2010-06-02 18:05:12 -0400

    added mysql install file for the plugin

---
+++ plugins/socnet/sql/mysql_install.php
@@ -0,0 +1,13 @@
+<?php 
+
+$_SQL[] = "
+	CREATE TABLE {$_TABLES['user_socnetinfo']} (
+	 uid INT NOT NULL,
+	 enabled TINYINT NOT NULL DEFAULT  '1',
+	 private TINYINT NOT NULL DEFAULT  '1',
+	 acceptinvites TINYINT NOT NULL DEFAULT  '1',
+	 show_profile TINYINT NOT NULL DEFAULT  '1',
+	PRIMARY KEY (uid)
+) ENGINE = MYISAM ;"
+
+?>

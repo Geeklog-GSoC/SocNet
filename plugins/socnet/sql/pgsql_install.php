2b0cc52b475a001655774c62df48af45e6c2c11d
stan <stan@spalatnik.com>
2010-06-04 17:00:51 -0400

    more plugin functions and added template files for plugin

---
+++ plugins/socnet/sql/pgsql_install.php
@@ -0,0 +1,13 @@
+<?php 
+
+$_SQL[] = "
+	CREATE TABLE {$_TABLES['user_socnetinfo']} (
+	 uid INT NOT NULL,
+	 enabled SMALLINT NOT NULL DEFAULT  '1',
+	 private SMALLINT NOT NULL DEFAULT  '1',
+	 acceptinvites SMALLINT NOT NULL DEFAULT  '1',
+	 show_profile SMALLINT NOT NULL DEFAULT  '1',
+	PRIMARY KEY (uid)
+);"
+
+?>
---
+++ public_html/admin/plugins/socnet/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once('../../../lib-common.php');
+
+//check for security
+
+$display = COM_siteHeader();
+$T = new Template($_CONF['path'] . 'plugins/socnet/templates');
+$T->set_file('page', 'admin.thtml');
+$T->set_var('header', $LANG_PL00['plugin']);
+$T->set_var('site_url',$_CONF['site_url']);
+$T->set_var('plugin','socnet');
+// your code goes here
+$T->parse('output','page');
+$display .= $T->finish($T->get_var('output'));
+$display .= COM_siteFooter();
+
+echo $display;
+?>
